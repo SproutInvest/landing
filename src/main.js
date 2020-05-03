223100fda480de20fa9042f326d2d52697338b3c
Srujan Routhu <srujan.routhu@columbia.edu>
2020-05-03 16:35:11 +0530

    error fix

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import SocialSharing from 'vue-social-sharing'
 
 import App from './App.vue'
 import i18n from './i18n'
@@ -19,8 +18,6 @@ Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
-
-Vue.use(SocialSharing)
 
 new Vue({
   i18n,
