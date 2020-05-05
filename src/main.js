e042dc20e54702d2306a4191fd3a56c1faa0cb7b
Srujan Routhu <srujan.routhu@columbia.edu>
2020-05-05 15:30:59 +0530

    updated from master

--- src/main.js
+++ src/main.js
@@ -6,10 +6,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-<<<<<<< HEAD
-=======
 import VueAnalytics from 'vue-analytics'
->>>>>>> e5a947c0e51b245b27c2c57a9e7e27305f219bf2
 
 import App from './App.vue'
 import i18n from './i18n'
@@ -25,14 +22,10 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 
-<<<<<<< HEAD
-=======
 Vue.use(VueAnalytics, {
   id: 'UA-164524931-1',
 })
 
-,
->>>>>>> e5a947c0e51b245b27c2c57a9e7e27305f219bf2
 new Vue({
   i18n,
   render: h => h(App),
